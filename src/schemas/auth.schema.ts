ca90c8fd2e68302b8c9d638f50518a206ea96bc9
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-01-06 23:12:55 -0500

    auth schema added: register schema added

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,35 @@
+import { TypeOf, z } from 'zod';
+
+export const registerSchema = z.object({
+  body: z
+    .object({
+      username: z
+        .string({
+          required_error: 'Username is required',
+          invalid_type_error: 'Username must be a string',
+        })
+        .min(5, { message: 'Username must contain at least 5 characters' })
+        .max(15, { message: 'The username must contain a maximum of 15 characters' }),
+      email: z.string({ required_error: 'Email is required' }).email({ message: 'The email is not valid' }),
+      password: z
+        .string({
+          required_error: 'Password is required',
+          invalid_type_error: 'Password must be a string',
+        })
+        .min(6, { message: 'Password must contain at least 6 characters' })
+        .max(15, { message: 'The password must contain a maximum of 15 characters' }),
+      confirmPassword: z
+        .string({
+          required_error: 'Confirm password is required',
+          invalid_type_error: 'Confirm password must be a string',
+        })
+        .min(6, { message: 'Confirm password must contain at least 6 characters' })
+        .max(15, { message: 'The confirm password must contain a maximum of 15 characters' }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Passwords do not match',
+      path: ['passwordConfirmation'],
+    }),
+});
+
+export type RegisterInput = TypeOf<typeof registerSchema>['body'];
