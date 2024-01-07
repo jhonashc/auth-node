e23bff314b02567b50da13c2270e5ebc99886933
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-01-06 22:02:35 -0500

    envs config updated: database and prisma orm variables added

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -5,4 +5,9 @@ import { get } from 'env-var';
 export const envs = {
   NODE_ENV: get('NODE_ENV').required().asString(),
   PORT: get('PORT').required().asPortNumber(),
+  MYSQL_ROOT_PASSWORD: get('MYSQL_ROOT_PASSWORD').required().asString(),
+  MYSQL_DATABASE: get('MYSQL_DATABASE').required().asString(),
+  MYSQL_HOST: get('MYSQL_HOST').required().asString(),
+  MYSQL_PORT: get('MYSQL_PORT').required().asPortNumber(),
+  DATABASE_URL: get('DATABASE_URL').required().asString(),
 };
