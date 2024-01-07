baf614cfbd28a2d984763b4ae39db17fd3ac3023
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-01-06 23:23:42 -0500

    auth controller updated: login added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-import { RegisterInput } from '../schemas';
+import { LoginInput, RegisterInput } from '../schemas';
 import { AuthService } from '../services';
 
 export class AuthController {
@@ -12,6 +12,15 @@ export class AuthController {
     this.authService
       .register(registerInput)
       .then((registerResponse) => res.status(201).json(registerResponse))
+      .catch((error) => next(error));
+  };
+
+  login = (req: Request<{}, {}, LoginInput>, res: Response, next: NextFunction) => {
+    const loginInput: LoginInput = req.body;
+
+    this.authService
+      .login(loginInput)
+      .then((loginResponse) => res.json(loginResponse))
       .catch((error) => next(error));
   };
 }
